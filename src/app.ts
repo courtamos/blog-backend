1db3c2045da3d9cd8df9f7ee34f218095296b4cf
Courtney Amos <c.amos@shaw.ca>
2021-08-19 11:08:36 -0600

    connect mongodb database

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import express, { Application, Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config();
 const app: Application = express();
+
+const db = process.env.MONGO_URI || '';
+const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(db, 
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  .then(() => console.log('MongoDB is now connected...'))
+  .catch(err => console.log(err));
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('Hello');
 });
 
-app.listen(5000, () => console.log('Server running'));
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
