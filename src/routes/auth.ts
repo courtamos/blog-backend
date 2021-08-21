7f785e3a1bd57ddfc405fe682cea315d48a02200
Courtney Amos <c.amos@shaw.ca>
2021-08-21 15:50:31 -0600

    implement user auth for login

---
+++ src/routes/auth.ts
@@ -0,0 +1,54 @@
+import { Router, Request, Response, NextFunction } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import UserModel from "../models/User";
+
+dotenv.config();
+
+const router = Router();
+const jwtSecret = process.env.JWTSECRET || "";
+
+// @route POST /auth
+// @desc auth user
+// @access public
+router.post("/", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ msg: "Please ensure all fields are filled out" });
+  }
+
+  const user = await UserModel.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ msg: "User does not exist" });
+  }
+
+  // validate password
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ msg: "Invalid password, try again" });
+  }
+
+  jwt.sign({ id: user.id }, jwtSecret, (err: any, token: any) => {
+    try {
+      res.json({
+        token,
+        user: {
+          id: user.id,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          subscribed: user.subscribed,
+          type: user.type,
+        },
+      });
+    } catch (err) {
+      throw err;
+    }
+  });
+});
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import posts from "./posts";
 import users from "./users";
+import auth from "./auth";
 
 const router = Router();
 
 router.use("/posts", posts);
 router.use("/users", users);
+router.use("/auth", auth);
 
 export default router;
