fa9ac9a213c762b3005cf084d7ccab7b350df91b
Courtney Amos <c.amos@shaw.ca>
2021-08-21 16:04:11 -0600

    create middleware for auth

---
+++ src/middleware/auth.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+const jwtSecret = process.env.JWTSECRET || "";
+
+export const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.header("x-auth-token") || "";
+
+  if (!token) {
+    res.status(401).json({ msg: "Not authorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(400).json({ msg: "Token invalid" });
+  }
+};
